aca94632a00a5193959735778ab527a9b444aa61
polymurph <eddyed.k@gmail.com>
2023-03-21 10:02:33 +0100

    renamed variable

--- Firmware/bsl/reflow_oven_bsl.c
+++ Firmware/bsl/reflow_oven_bsl.c
@@ -1,33 +1,33 @@
 #include "reflow_oven_bsl.h"
 #include "modules.h"
 
-void reflowOvenInitHw(reflowBSL_t *oven)
+void reflowOvenInitHw(reflowBSL_t *oven_bsl)
 {
-	delayInitMs(oven->clockSpeed, 1000); // Clock Freq and Divider for ARM library 
+	delayInitMs(oven_bsl->clockSpeed, 1000); // Clock Freq and Divider for ARM library 
 	
 	// Reset pin
-	pinConfig(oven->resetPin, input, def_stage, pullDown, def_speed);
+	pinConfig(oven_bsl->resetPin, input, def_stage, pullDown, def_speed);
 
 	// init uart	
-	usartInit(	oven->uartChannel, 
-				oven->uartTx, 
-				oven->uartRx, 
-				oven->baudRate,
+	usartInit(	oven_bsl->uartChannel, 
+				oven_bsl->uartTx, 
+				oven_bsl->uartRx, 
+				oven_bsl->baudRate,
 				eight, 
 				NO_PARITY_CTRL,
 				noFlowControl);
 
 	setupInit(); // This is the sescond call of System init the assebly start code is calling it before the main.	
 	
-	print_Usart(oven->uartChannel, ASCII_clear); 
+	print_Usart(oven_bsl->uartChannel, ASCII_clear); 
 
 	// Init modules here
-	display_init(oven);
-	door_init(oven);
-	fan_init(oven);
-	heater_init(oven);
-	temperature_init(oven);
-	userIO_init(oven);
+	display_init(oven_bsl);
+	door_init(oven_bsl);
+	fan_init(oven_bsl);
+	heater_init(oven_bsl);
+	temperature_init(oven_bsl);
+	userIO_init(oven_bsl);
 	
-	print_Usart(oven->uartChannel, "Reflow oven init successful\n\r"); 	
+	print_Usart(oven_bsl->uartChannel, "Reflow oven init successful\n\r"); 	
 }
